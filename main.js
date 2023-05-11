7b23539f94cec0fc6c0eadd48f2360c4dbc14a7f
Kobe Van Dyck <kobevandyck2003@gmail.com>
2023-05-11 10:34:55 +0200

    visuals

--- main.js
+++ main.js
@@ -1,74 +1,322 @@
-import './style.css';
+
+
+import {getAudioContext, getAudioSource, getStart, buttonPressed} from "./midi.js";
 
 
 
+const container = document.getElementById('container');
+const canvas = document.getElementById('canvas1');
+const file = document.getElementById('fileupload');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const ctx = canvas.getContext('2d');
 
-  // core version + navigation, pagination modules:
-  import Swiper, { Navigation, Pagination } from 'swiper';
-  // import Swiper and modules styles
-  import 'swiper/css';
-  import 'swiper/css/navigation';
-  import 'swiper/css/pagination';
+/* 
+ctx.shadowOffsetX = 15;
+ctx.shadowOffsetY = 10;
+ctx.shadowBlur = 5;
+ctx.shadowColor = 'black';
+*/
 
-  const swiper = new Swiper('.swiper', {
-    // configure Swiper to use modules
-    modules: [Navigation, Pagination], 
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+let analyser;
+/*
+container.addEventListener('click', function(){
+  const audio1 = document.getElementById('audio1');
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  
-  
-  });
+ 
+ const audioContext = new AudioContext();
+
+  audio1.play();
+  audioSource = audioContext.createMediaElementSource(audio1);
+  analyser = audioContext.createAnalyser();
+  audioSource.connect(analyser);
+  analyser.connect(audioContext.destination);
+
+  analyser.fftSize = 1024;
+  const bufferLenght = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLenght);
 
 
-  function location() {
-    setTimeout(() => {
-      getAddressFromCoordinates();
-    }, 2000);
-}
+  const barWidth = 10;
+  let barHeight;
+  let x;
 
-function error(){
-  document.getElementById('address').textContent = "GAST UW DATA SUCKED";
-  document.getElementById('address').style.color = "rgb(198, 66, 66)";
-};
-
- const getAddressFromCoordinates = async () =>{
-    const response = await fetch (`https://api.mapbox.com/geocoding/v5/mapbox.places/4.481399602726015,51.025710874850546.json?&access_token=pk.eyJ1Ijoic2Fpbm8iLCJhIjoiY2xldjZnbDIyMXg2ZDN2cDRkZG5xaWF1ZyJ9.ppAUFDUs6U1j6c3l0UbKZA`);
-    const data = await response.json();
-    document.getElementById("loader").style.display = "none";
-    try{
-      const address = data.features[0].place_name;
-      document.getElementById('address').textContent = address;
-    } catch(err){
-      error();
-    }
-}
-
-await location();
-
-  async function footer() {
-    // Display the loading spinner
-    document.getElementById("loader").style.display = "block";
-    
-    try {
-      // Geocode the address
-      await geocodeAddress(address);
-
-
-    } finally {
-      // Hide the loading spinner
-      document.getElementById("loader").style.display = "none";
-    }
+  function animate(){
+    x = 0;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    analyser.getByteFrequencyData(dataArray);
+    drawVisualiser(bufferLenght, x, barWidth, barHeight, dataArray);
+    requestAnimationFrame(animate);
   }
+  animate();
+
+});
+*/
+let bufferLenght
+let dataArray
+let audioContext
+let audioSource
+function startVisuals() {
+   audioContext = getAudioContext();
+   audioSource = getAudioSource();
+  analyser = audioContext.createAnalyser(); /* vanaf deze line alles laten staan */
+  audioSource.connect(analyser);
+  analyser.connect(audioContext.destination);
+  analyser.fftSize = 2048;
+  bufferLenght = analyser.frequencyBinCount;
+  dataArray = new Uint8Array(bufferLenght);
+
+
+  
+
+  
+}
+const barWidth = 10;
+  let barHeight;
+  let x;
+
+function animate(){
+  if (analyser) {
+  console.log('animate')
+  x = 0;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  analyser.getByteFrequencyData(dataArray);
+  currentVisualizer(bufferLenght, x, barWidth, barHeight, dataArray);
+}
+requestAnimationFrame(animate);
+}
+animate();
+
+getStart(startVisuals)
+
+let currentVisualizer = null
+const visualisers = [drawVisualiser1, drawVisualiser2, drawVisualiser3, drawVisualiser4]
+function pickVisualizer () {
+  currentVisualizer = visualisers[Math.floor(Math.random() * visualisers.length)]
+}
+pickVisualizer()
+
+buttonPressed(pickVisualizer)
+
+
+function drawVisualiser1(bufferLenght, x, barWidth, barHeight, dataArray){
+
+ 
+
+
+/* VORTEX */
+
+for (let i = 0; i < bufferLenght; i++) {
+  barHeight = dataArray[i] * 2.5;
+  ctx.save();
+  ctx.translate(canvas.width/2, canvas.height/2);
+  ctx.rotate(i * 4.135);
+  const hue = 300 + i * 1  ;
+  ctx.fillStyle = 'hsl(' + hue + ',100%, 50%)';
+  ctx.beginPath();
+  ctx.arc(10, barHeight*1, barHeight*2.5, 0, Math.PI/ 4);
+  ctx.fill();
+  ctx.stroke();
+  x += barWidth;
+  ctx.restore();
+}
+
+
+/* BIT STAAF MIDDLE CIRCLE OFZO */
+
+for (let i = 0; i < bufferLenght; i++) {
+  barHeight = dataArray[i] * 2;
+  ctx.save();
+  ctx.translate(canvas.width/2, canvas.height/2);
+  ctx.rotate(i * 4.135);
+  const hue = i * 5  ;
+  ctx.fillStyle = 'hsl(' + hue + ',100%, 60%)';
+  ctx.fillRect(0, 0, barWidth, barHeight);
+  x += barWidth;
+  ctx.restore();
+} 
+
+
+
+/* BUBBLES */
+/*
+for (let i = 0; i < bufferLenght; i++) {
+  barHeight = dataArray[i] * 1.4;
+  ctx.save();
+  ctx.translate(canvas.width/2, canvas.height/2);
+  ctx.rotate(i * bufferLenght * -3.991);
+  const hue = i * 2  ;
+  ctx.fillStyle = 'hsl(' + hue + ',100%, 50%)';
+  ctx.beginPath();
+  ctx.arc(0, barHeight, barHeight/10, 0, Math.PI * 2);
+  ctx.arc(0, barHeight/1.5, barHeight/20, 0, Math.PI * 2);
+  ctx.arc(0, barHeight/2, barHeight/30, 0, Math.PI * 2);
+  ctx.arc(0, barHeight/3, barHeight/40, 0, Math.PI * 2);
+  ctx.fill();
+  x += barWidth;
+  ctx.restore();
+} 
+*/
+
+
+
+} 
+
+
+  /* console.log(bufferLenght, barHeight, dataArray) */
+
+
+
+
+  function drawVisualiser2(bufferLenght, x, barWidth, barHeight, dataArray){
+
+ 
+
+
+    /* VORTEX */
+    
+    for (let i = 0; i < bufferLenght; i++) {
+      barHeight = dataArray[i] * 2.5;
+      ctx.save();
+      ctx.translate(canvas.width/2, canvas.height/2);
+      ctx.rotate(i * 4.135);
+      const hue = 300 + i * 1  ;
+      ctx.fillStyle = 'hsl(' + hue + ',100%, 50%)';
+      ctx.beginPath();
+      ctx.arc(10, barHeight*1, barHeight*2.5, 0, Math.PI/ 4);
+      ctx.fill();
+      ctx.stroke();
+      x += barWidth;
+      ctx.restore();
+    }
+    
+    
+    
+    /* BUBBLES */
+    
+    for (let i = 0; i < bufferLenght; i++) {
+      barHeight = dataArray[i] * 1.4;
+      ctx.save();
+      ctx.translate(canvas.width/2, canvas.height/2);
+      ctx.rotate(i * bufferLenght * -3.991);
+      const hue = i * 2  ;
+      ctx.fillStyle = 'hsl(' + hue + ',100%, 50%)';
+      ctx.beginPath();
+      ctx.arc(0, barHeight, barHeight/10, 0, Math.PI * 2);
+      ctx.arc(0, barHeight/1.5, barHeight/20, 0, Math.PI * 2);
+      ctx.arc(0, barHeight/2, barHeight/30, 0, Math.PI * 2);
+      ctx.arc(0, barHeight/3, barHeight/40, 0, Math.PI * 2);
+      ctx.fill();
+      x += barWidth;
+      ctx.restore();
+    } 
+    
+    
+    
+    
+    } 
+
+
+
+    function drawVisualiser3(bufferLenght, x, barWidth, barHeight, dataArray){
+
+ 
+
+
+      /* VORTEX */
+      
+      for (let i = 0; i < bufferLenght; i++) {
+        barHeight = dataArray[i] * 2.5;
+        ctx.save();
+        ctx.translate(canvas.width/2, canvas.height/2);
+        ctx.rotate(i * 4.135);
+        const hue = 300 + i * 1  ;
+        ctx.fillStyle = 'hsl(' + hue + ',100%,' + barHeight/3 +  '%)';
+        ctx.beginPath();
+        ctx.arc(10, barHeight*1, barHeight*2.5, 0, Math.PI/ 4);
+        ctx.fill();
+        ctx.stroke();
+        x += barWidth;
+        ctx.restore();
+      }
+      
+
+      for (let i = 0; i < bufferLenght; i++) {
+        barHeight = dataArray[i] * 2;
+        ctx.save();
+        ctx.translate(canvas.width/2, canvas.height/2);
+        ctx.rotate(i * Math.PI * 4 / bufferLenght);
+        const hue = 300 + i * 1  ;
+        ctx.fillStyle = 'hsl(' + hue + ',100%,' + barHeight/3 +  '%)';
+        ctx.lineWidth = barHeight/10;
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+        ctx.lineTo(0, barHeight);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(0, barHeight + barHeight/5, barHeight/20, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.arc(0, barHeight + barHeight/5, barHeight/10, 0, Math.PI * 2);
+        ctx.fill();
+        x += barWidth;
+        ctx.restore();
+      } 
+      
+      
+      
+      } 
+    
+
+
+
+      function drawVisualiser4(bufferLenght, x, barWidth, barHeight, dataArray){
+
+       /* VORTEX */
+      
+       for (let i = 0; i < bufferLenght; i++) {
+        barHeight = dataArray[i] * 2.5;
+        ctx.save();
+        ctx.translate(canvas.width/2, canvas.height/2);
+        ctx.rotate(i * 4.135);
+        const hue = 300 + i * 1  ;
+        ctx.fillStyle = 'hsl(' + hue + ',100%,' + barHeight/3 +  '%)';
+        ctx.beginPath();
+        ctx.arc(10, barHeight*1, barHeight*2.5, 0, Math.PI/ 4);
+        ctx.fill();
+        ctx.stroke();
+        x += barWidth;
+        ctx.restore();
+      }
+
+
+
+
+
+for (let i = 0; i < bufferLenght; i++) {
+  barHeight = dataArray[i] * 1.2;
+  ctx.save();
+  ctx.translate(canvas.width/2, canvas.height/2);
+  ctx.rotate(i * bufferLenght / 1.2);
+  ctx.lineWidth = barHeight/7;
+  const hue = i * 2  ;
+  ctx.fillStyle = 'hsl(' + hue + ',100%, 50%)';
+  ctx.beginPath();
+  ctx.moveTo(0, barHeight/1.1);
+  ctx.lineTo(barHeight/1.1, barHeight);
+  ctx.stroke();
+  x += barWidth;
+  ctx.restore();
+} 
+
+
+
+
+    
+        
+        
+        } 
+    
+
+    
 
---
+++ midi.js
@@ -0,0 +1,214 @@
+let audioContext = new AudioContext();
+let audiSource = null;
+
+export function getAudioContext() {
+  return audioContext;
+}
+
+export function getAudioSource() {
+  return audiSource;
+}
+
+let startVisuals = null;
+
+export function getStart(func) {
+  startVisuals = func;
+}
+
+let butternPress = null
+
+export function buttonPressed(func) {
+  console.log('buttonPressed', func)
+  butternPress = func;
+}
+
+// Load the samples
+const sampleUrls = ["bitboy.mp3", "Glowing.wav", "On.mp3", "intro.mp3"];
+const buffers = [];
+
+sampleUrls.forEach((url) => {
+  const request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
+
+  request.onload = function () {
+    audioContext.decodeAudioData(request.response, function (decodedData) {
+      buffers.push(decodedData);
+    });
+  };
+
+  request.send();
+});
+
+
+
+/*
+
+// Handle MIDI messages
+function onMIDIMessage(event) { 
+  // Handle only note on messages (event.data[0] = 144)
+  if (event.data[0] == 144) {
+    console.log(event.data[1]);
+    // Determine which sample to play based on MIDI note number
+    const noteNumber = event.data[1];
+    let bufferIndex = 0;
+    
+    if (noteNumber == 72) {
+      bufferIndex = 1;
+    }
+
+    if (noteNumber == 71) {
+      bufferIndex = 2;
+    }
+
+    if (noteNumber == 69) {
+      bufferIndex = 3;
+    }
+
+    if (noteNumber == 67) {
+      bufferIndex = 4;
+    }
+
+    
+    
+    
+    // const bufferIndex = noteNumber % buffers.length;
+
+    // Stop the previously playing sample
+    if (audiSource) {
+      audiSource.stop();
+    }
+    // Play the new sample
+    audiSource = audioContext.createBufferSource();
+    audiSource.buffer = buffers[bufferIndex];
+    audiSource.connect(audioContext.destination);
+    audiSource.start(0);
+    startVisuals();
+  }
+}
+
+*/
+
+// WEBSOCKET
+
+const url = "ws://192.168.100.1:1880/visualiser";
+  
+function wsConnect() {
+    console.log("connect", url);
+    const ws = new WebSocket(url);
+
+    ws.onmessage = function (msg) {
+        console.log(msg.data);
+
+        butternPress()
+        
+
+        var bufferIndex;
+        if (msg.data === "rood") {
+          bufferIndex = 0;
+        }
+      
+        if (msg.data === "wit") {
+          bufferIndex = 1;
+        }
+      
+        if (msg.data === "groen") {
+          bufferIndex = 2;  
+        }
+      
+        if (msg.data === "blauw") {
+          bufferIndex = 3;
+        }
+      
+        console.log("KNOP");
+      // Stop the previously playing sample
+      if (audiSource) {
+        audiSource.stop();
+      }
+      
+      // Play the new sample
+      audiSource = audioContext.createBufferSource();
+      audiSource.buffer = buffers[bufferIndex];
+      audiSource.connect(audioContext.destination);
+      audiSource.start(0);
+      startVisuals();
+        
+    }
+
+    ws.onopen = function () {
+        console.log("Connected");
+    }
+
+    ws.onclose = function () {
+        setTimeout(wsConnect, 3000);
+    }
+
+    ws.disconnect = function () {
+        console.log("Disconnected");
+    }
+}
+
+function update() {
+    if (ws) { ws.send(output); }
+}
+
+wsConnect();
+
+let buttonPressCount = 0;
+
+document.addEventListener('keydown', function(event) {
+  // Get the key code
+  const keyCode = event.keyCode;
+  console.log(event);
+
+  // Determine which sample to play based on the key code
+  let bufferIndex = 5;
+
+  if (keyCode === 65) {
+    bufferIndex = 0;
+  }
+
+  if (keyCode === 90) {
+    bufferIndex = 1;
+  }
+
+  if (keyCode === 69) {
+    bufferIndex = 2;  
+  }
+
+  if (keyCode === 82) {
+    bufferIndex = 3;
+  }
+
+  if (keyCode === 84) {
+    bufferIndex = 4;
+  }
+
+  if (bufferIndex != 5) {
+    console.log("KNOP");
+    // Stop the previously playing sample
+    if (audiSource) {
+      audiSource.stop();
+    }
+
+    // Play the new sample
+    audiSource = audioContext.createBufferSource();
+    audiSource.buffer = buffers[bufferIndex];
+    audiSource.connect(audioContext.destination);
+    audiSource.start(0);
+    startVisuals();
+    
+    // Increment the button press counter
+    buttonPressCount++;
+
+    // Check if the button has been pressed three times
+    if (buttonPressCount === 4 ) {
+      // Refresh the page
+      location.reload();
+    }
+  }
+});
+
+ 
+
+//console.log(onMIDIMessage);
